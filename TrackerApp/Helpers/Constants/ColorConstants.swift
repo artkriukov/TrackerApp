f07c15d9b1e1a2affe57bb466e4b8d4b35ef1c14
Artem <art.kriukov@gmail.com>
2025-05-18 17:41:16 +0300

    feat: add Selection colors

---
+++ TrackerApp/Helpers/Constants/ColorConstants.swift
@@ -0,0 +1,33 @@
+//
+//  ColorConstants.swift
+//  TrackerApp
+//
+//  Created by Artem Kriukov on 18.05.2025.
+//
+
+import UIKit
+
+enum ColorConstants {
+    
+    enum SelectionColors {
+        static let colorSelection1 = UIColor(hexString: "FD4C49")
+        static let colorSelection2 = UIColor(hexString: "FF881E")
+        static let colorSelection3 = UIColor(hexString: "007BFA")
+        static let colorSelection4 = UIColor(hexString: "6E44FE")
+        static let colorSelection5 = UIColor(hexString: "33CF69")
+        static let colorSelection6 = UIColor(hexString: "E66DD4")
+        static let colorSelection7 = UIColor(hexString: "F9D4D4")
+        static let colorSelection8 = UIColor(hexString: "34A7FE")
+        static let colorSelection9 = UIColor(hexString: "46E69D")
+        static let colorSelection10 = UIColor(hexString: "35347C")
+        static let colorSelection11 = UIColor(hexString: "FF674D")
+        static let colorSelection12 = UIColor(hexString: "FF99CC")
+        static let colorSelection13 = UIColor(hexString: "F6C48B")
+        static let colorSelection14 = UIColor(hexString: "7994F5")
+        static let colorSelection15 = UIColor(hexString: "832CF1")
+        static let colorSelection16 = UIColor(hexString: "AD56DA")
+        static let colorSelection17 = UIColor(hexString: "8D72E6")
+        static let colorSelection108 = UIColor(hexString: "2FD058")
+    }
+    
+}
