aa756d3a0cbe3e0bda802c00d355a132afdbbf77
Artem <art.kriukov@gmail.com>
2025-05-18 14:06:45 +0300

    Initial project

---
+++ TrackerApp/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackerApp
+//
+//  Created by Artem Kriukov on 18.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
