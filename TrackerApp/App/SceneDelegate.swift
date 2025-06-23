e83a59986c41830498669f336580f47f6b581c4a
Artem <art.kriukov@gmail.com>
2025-06-23 14:46:23 +0300

    refactor: fix scene delegate

--- TrackerApp/App/SceneDelegate.swift
+++ TrackerApp/App/SceneDelegate.swift
@@ -15,11 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-//        if !UserDefaults.standard.bool(forKey: "onboardingCompleted") {
+        if !UserDefaults.standard.bool(forKey: "onboardingCompleted") {
             window.rootViewController = OnboardingViewController()
-//        } else {
-//            window.rootViewController = TabBarViewController() 
-//        }
+        } else {
+            window.rootViewController = TabBarViewController() 
+        }
         
         window.makeKeyAndVisible()
         self.window = window
